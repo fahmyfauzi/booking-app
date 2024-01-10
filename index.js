05d74a184b15b4d5d661399ae9ba024a245df4ee
Fahmy <fahmyfauzii@gmail.com>
2024-01-10 13:53:24 +0700

    mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+
+// Fungsi untuk menghubungkan ke MongoDB
+const connect = async () => {
+  try {
+    // Menggunakan metode connect dari Mongoose untuk menghubungkan ke MongoDB
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connect to mongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Event handler untuk event disconnected pada koneksi MongoDB
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+// Event handler untuk event connected pada koneksi MongoDB
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
+});
 
 app.listen(3000, () => {
+  connect();
   console.log(`Connected to backend!`);
 });
